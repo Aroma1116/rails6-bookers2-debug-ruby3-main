12dc2f45d84571398325c67d10eff2c50e7849da
Aroma1116 <aroma1116@outlook.jp>
2022-12-09 00:41:25 +0000

    応用課題３完了

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,25 @@
+class BookCommentsController < ApplicationController
+
+  def create
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    if @comment.save
+      redirect_to book_path(@book)
+    else
+      @error_comment = @comment
+      @user = @book.user
+      render 'books/show'
+    end
+  end
+
+  def destroy
+    BookComment.find(params[:id]).destroy
+    redirect_to book_path(params[:book_id])
+  end
+
+  private
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+end
