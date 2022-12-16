06ccac103d60a91bd1f47bc6ac6578a00a2b49a6
Aroma1116 <aroma1116@outlook.jp>
2022-12-16 02:16:02 +0000

    完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :user_rooms, dependent: :destroy
   has_many :chats, dependent: :destroy
-  has?many :view_counts, dependent: :destroy
+  has_many :view_counts, dependent: :destroy
 
   has_one_attached :profile_image
 
